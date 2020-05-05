0bd8ef8ce3a8bf390b342cb7ec8ae8545fdcdc33
Jeremy Hilado <jhilado89@gmail.com>
2020-05-05 08:12:38 -0700

    init commit

---
+++ myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Jeremy/Documents/seir224/unit04/week11/labs/python-django-mini-project/myenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
